3a33f2d92bb693c758e400f9b5e3a47360ced121
Vitaly Zharov <VVZharov@sberbank.ru>
2021-12-07 17:47:56 +0500

    Заготовка под обзор трансформирующих операторов (part 1).

--- app/src/main/java/com/github/zharovvv/rxjavasandbox/rxjava/example/operators/TransformingOperatorsExample.kt
+++ app/src/main/java/com/github/zharovvv/rxjavasandbox/rxjava/example/operators/TransformingOperatorsExample.kt
@@ -1,4 +1,20 @@
 package com.github.zharovvv.rxjavasandbox.rxjava.example.operators
 
+import io.reactivex.Observable
+
 class TransformingOperatorsExample {
+
+    /**
+     * [flatMap] - Возвращает Observable, который генерирует элементы на основе применения функции,
+     * которую вы предоставляете к каждому элементу, испускаемому источником ObservableSource,
+     * где эта функция возвращает ObservableSource,
+     * а затем объединяет полученные ObservableSources и генерирует результаты этого слияния.
+     */
+    fun flatMapExample() {
+        val observable: Observable<Char> = Observable.just("str1", "str2")
+            .flatMap { string: String ->
+                val charArray = string.toCharArray().toTypedArray()
+                Observable.fromArray(*charArray)
+            }
+    }
 }
