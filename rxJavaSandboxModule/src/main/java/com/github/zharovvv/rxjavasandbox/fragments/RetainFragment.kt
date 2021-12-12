671e6fa694ba895623ac1afd8c133cf00d527531
Vitaly Zharov <zharov.vitalik@gmail.com>
2021-12-12 13:38:00 +0500

    Ренейм названия модуля app->rxJavaSandboxModule

--- rxJavaSandboxModule/src/main/java/com/github/zharovvv/rxjavasandbox/fragments/RetainFragment.kt
+++ rxJavaSandboxModule/src/main/java/com/github/zharovvv/rxjavasandbox/fragments/RetainFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 
 abstract class RetainFragment : Fragment() {
 
